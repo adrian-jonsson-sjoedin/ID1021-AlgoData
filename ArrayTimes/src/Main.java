1e64a15ea62745d6509020decd6767fd01b57a13
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-08-29 17:12:28 +0200

    add code for intro task

---
+++ ArrayTimes/src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+//        forLoopTime();
+//        System.out.println("-------------");
+//        arraySumTime();
+//        System.out.println("-------------");
+        System.out.println(access(10));
+    }
+
+    public static void arraySumTime() {
+        int[] given = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            long t0 = System.nanoTime();
+            sum += given[i];
+            long t1 = System.nanoTime();
+            System.out.println(" resolution " + (t1 - t0) + " nanoseconds");
+        }
+    }
+
+    public static void forLoopTime() {
+        for (int i = 0; i < 10; i++) {
+            long n0 = System.nanoTime();
+            long n1 = System.nanoTime();
+            System.out.println(" resolution " + (n1 - n0) + " nanoseconds");
+        }
+    }
+
+    private static double access(int n) {
+        int k = 1_000_000;
+        int l = n;
+        //creates an instance of Random to use for generating random numbers
+        Random rnd = new Random();
+        int[] indx = new int[l];
+        // fill the indx array with random number from 0 to n (not including n)
+        for (int i = 0; i < n; i++) {
+            indx[i] = rnd.nextInt(n);
+        }
+        int[] array = new int[n];
+// fill the array with dummy values (why not 1)
+        for (int i = 0; i < n; i++) {
+            array[i] = 1;
+        }
+        int sum = 0;
+        //system time in ns when the arrays have been created. Used to establish a baseline time (i.e. zero)
+        long t0 = System.nanoTime();
+        for (int j = 0; j < k; j++) {
+            for (int i = 0; i < l; i++) {
+// access the array with the index given by indx[i]
+// sum up the result
+                sum += array[indx[i]];
+            }
+        }
+        long t_access = (System.nanoTime() - t0);
+        t0 = System.nanoTime();
+        int dummy = 0;
+// do the same loop iteration but only do a dummy add operation
+        for (int j = 0; j < k; j++) {
+            for (int i = 0; i < l; i++) {
+                dummy = 1 + 1;
+            }
+        }
+        long t_dummy = (System.nanoTime() - t0);
+        return ((double) (t_access - t_dummy)) / ((double) k * (double) l);
+    }
+}
