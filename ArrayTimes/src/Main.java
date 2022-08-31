b48a9e3decfe7c8a7516771cec23eaee5c8f2d42
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-08-31 11:15:32 +0200

    solve task 1 in a better way. split up methods into multiple smaller methods

--- ArrayTimes/src/Main.java
+++ ArrayTimes/src/Main.java
@@ -2,58 +2,101 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//        callAccessWithLargerAndLargerN(1000);
-        System.out.println("array size n = 10, nr of searches = 10 000: one element access takes "
-                + access(10, 10_000) + "ns");
-        System.out.println("array size n = 100, nr of searches = 10 000: one element access takes "
-                + access(100, 10_000) + "ns");
-        System.out.println("array size n = 1000, nr of searches = 10 000: one element access takes "
-                + access(1000, 10_000) + "ns");
-        System.out.println("array size n = 10 000, nr of searches = 10 000: one element access takes "
-                + access(10_000, 10_000) + "ns");
-        System.out.println("array size n = 50 000, nr of searches = 50 000: one element access takes "
-                + access(50_000, 50_000) + "ns");
-        System.out.println("array size n = 100 000, nr of searches = 10 000: one element access takes "
-                + access(100_000, 50_000) + "ns");
+      //  System.out.println(randomArrayElementAccess(10000, 1000000));
+    benchmark(1000,100000);
+    }
+
+    public static void benchmark(int n, int loops) {
+        int j = 1;
+        for (int i = 1; i <= n; i = 10 * j) {
+            j++;
+            randomArrayElementAccess(n,loops);
+            System.out.println("n = " + i + "--> " + randomArrayElementAccess(n, loops) + "ns");
+        }
+    }
+
+    private static double randomArrayElementAccess(int arraySize, int loops) {
+        int[] randomArray = randomArray(arraySize);
+        int[] randomArrayIndex = randomArraysIndex(randomArray);
+        int sum = 0;
+        long t0 = System.nanoTime();
+        for (int j = 0; j < loops; j++) {
+            for (int i = 0; i < arraySize; i++) {
+                sum += randomArray[randomArrayIndex[i]];
+            }
+        }
+        long tAccess = (System.nanoTime() - t0);
+        //add dummy loop to be able to subtract the time it takes for the program to do the loop, leaving just the time
+        //it takes for the array access.
+        t0 = System.nanoTime();
+        int dummy = 0;
+        for (int j = 0; j < loops; j++) {
+            for (int i = 0; i < arraySize; i++) {
+                dummy += 1;
+            }
+        }
+        long tDummy = (System.nanoTime() - t0);
+        return ((double) (tAccess - tDummy) / ((double) arraySize * (double) loops));
+    }
+
+    /**
+     * Creates an integer array populated by random values up to 10 times the length of the array
+     *
+     * @param arraySize is the length of the array
+     * @return an integer array
+     */
+    private static int[] randomArray(int arraySize) {
+        Random rnd = new Random();
+        int[] array = new int[arraySize];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt(10 * arraySize);
+        }
+        return array;
+    }
+
+    /**
+     * Creates an array whose element values are the index for another array but in random order
+     *
+     * @param randomArray the array for which we want to create an index array
+     * @return an integer array
+     */
+    private static int[] randomArraysIndex(int[] randomArray) {
+        int[] index = new int[randomArray.length];
+        Random rnd = new Random();
+        for (int j = 0; j < index.length; j++) {
+            index[j] = rnd.nextInt(randomArray.length);
+        }
+        return index;
     }
 
 
-    private static double access(int arraySize, int nrOfSearches) {
-        int k = 1_000_000;
-        int l = nrOfSearches;
-        //creates an instance of Random to use for generating random numbers
-        Random rnd = new Random();
-        int[] indx = new int[l];
-        // fill the indx array with random number from 0 to n (not including n)
-        for (int i = 0; i < l; i++) {
-            indx[i] = rnd.nextInt(arraySize);
-        }
+    private static double searchTime(int nrOfRounds, int nrOfSearchOperationsPerRound, int arraySize) {
+        int[] keys = new int[nrOfSearchOperationsPerRound];
         int[] array = new int[arraySize];
-// fill the array with dummy values (why not 1)
-        for (int i = 0; i < arraySize; i++) {
-            array[i] = 1;
+        Random rnd = new Random();
+        //fill the two arrays with random numbers
+        for (int j = 0; j < keys.length; j++) {
+            keys[j] = rnd.nextInt(10 * arraySize);
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt(10 * arraySize);
         }
         int sum = 0;
-        //system time in ns when the arrays have been created. Used to establish a baseline time (i.e. zero)
         long t0 = System.nanoTime();
-        for (int j = 0; j < k; j++) {
-            for (int i = 0; i < l; i++) {
-// access the array with the index given by indx[i]
-// sum up the result
-                sum += array[indx[i]];
+        long tTotal = 0;
+        for (int k = 0; k < nrOfSearchOperationsPerRound; k++) {
+            int key = keys[k];
+            for (int i = 0; i < arraySize; i++) {
+                if (array[i] == key) {
+                    sum++;
+                    break;
+                }
             }
         }
-        long t_access = (System.nanoTime() - t0);
-        t0 = System.nanoTime();
-        int dummy = 0;
-// do the same loop iteration but only do a dummy add operation
-        for (int j = 0; j < k; j++) {
-            for (int i = 0; i < l; i++) {
-                dummy = i + 1;
-            }
+        tTotal += (System.nanoTime() - t0);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
-        long t_dummy = (System.nanoTime() - t0);
-        return ((double) (t_access - t_dummy)) / ((double) k * (double) l); // divide by l to get the time for one element
-        //access
+        return ((double) tTotal / (nrOfSearchOperationsPerRound * nrOfRounds));
     }
 }
