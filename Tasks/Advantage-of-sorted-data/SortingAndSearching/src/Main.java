bf9a7e7b85e96f0ce20dcf1d881d7aa7ed80e24e
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-09-06 11:24:45 +0200

    fix bug in task 1 anmd start on task 2

--- Tasks/Advantage-of-sorted-data/SortingAndSearching/src/Main.java
+++ Tasks/Advantage-of-sorted-data/SortingAndSearching/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         //running benchmark test of unsorted array
-        for (int i = 100; i < 1000000; i += 100) {
-            System.out.println("array size = " + i + ", search time = " + benchmarkUnsortedSearch(i) + "ns");
+        for (int i = 100; i < 100_000; i += 100) {
+//            System.out.println("array size = " + i + ", search time = " + benchmarkUnsortedSearch(i) + "ns");
+            System.out.printf("%d\t %f%n",i, benchmarkUnsortedSearch(i));
+//            System.out.println(benchmarkUnsortedSearch(i));
         }
-
     }
-
     public static boolean searchUnsorted(int[] arrayToSearch, int key) {
         for (int i = 0; i < arrayToSearch.length; i++) {
             if (arrayToSearch[i] == key) {
@@ -27,16 +27,33 @@ public class Main {
      */
     public static double benchmarkUnsortedSearch(int maxArraySize) {
         Random rnd = new Random();
-        int[] arrayToSearch = new int[maxArraySize];
+        int[] arrayToSearch = createRandomArray(maxArraySize);
         double sum = 0;
-        for (int i = 0; i < 1_000_000; i++) {
-            int key = rnd.nextInt(maxArraySize);
+        for (int i = 0; i < 100_000; i++) {
+            int key = rnd.nextInt(100_000);
             long timeStart = System.nanoTime();
             searchUnsorted(arrayToSearch, key);
             sum += (double) (System.nanoTime() - timeStart) ;
         }
-        return sum / 1000000;
+        return sum / 100_000;
     }
 
+    public static int[] createSortedArray(int arraySize) {
+        Random rnd = new Random();
+        int[] array = new int[arraySize];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (rnd.nextInt(10)+1);
+        }
+        return array;
+    }
+
+    public static int[] createRandomArray(int maxArraySize) {
+        Random rnd = new Random();
+        int[] rndArray = new int[maxArraySize];
+        for (int i = 0; i < rndArray.length; i++) {
+            rndArray[i] = rnd.nextInt(100_000);
+        }
+        return rndArray;
+    }
 
 }
