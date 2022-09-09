f492ec084822db8962807dead6b174002d09df51
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-09-09 15:09:32 +0200

    assignment finished. two different methods for duplicate search implemented

--- Tasks/Advantage-of-sorted-data/SortingAndSearching/src/Main.java
+++ Tasks/Advantage-of-sorted-data/SortingAndSearching/src/Main.java
@@ -11,20 +11,41 @@ public class Main {
 //    }
 //       }
 //        System.out.printf("%f%n 16M",  benchmarkBinarySearch(64_000_000));
-        int[] a = new int[]{1, 2, 4, 6};
-        int[] b = new int[]{2, 3, 5, 6, 7};
-        System.out.printf("first array: ");
-        for (int i = 0; i < a.length; i++) {
-            System.out.printf("%d ", a[i]);
-        }
-        System.out.println();
-        System.out.printf("second array: ");
-        for (int i = 0; i < b.length; i++) {
-            System.out.printf("%d ", b[i]);
-        }
-        System.out.println();
-        betterDuplicateSearch(a, b);
 
+        //testing boundary values for better duplicate search
+//        int[] a = new int[]{1, 2, 4, 6};
+//        int[] b = new int[]{2, 3, 5,6,7,8};
+//        System.out.printf("first array: ");
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.printf("%d ", a[i]);
+//        }
+//        System.out.println();
+//        System.out.printf("second array: ");
+//        for (int i = 0; i < b.length; i++) {
+//            System.out.printf("%d ", b[i]);
+//        }
+//        System.out.println();
+//        betterDuplicateSearch(a, b);
+
+        //benchmark of the duplicate search
+        System.out.printf("%d\t %f%n", 100, benchmarkDuplicateSearch(100, 100));
+        System.out.printf("%d\t %f%n", 200, benchmarkDuplicateSearch(200, 200));
+        System.out.printf("%d\t %f%n", 400, benchmarkDuplicateSearch(400, 400));
+        System.out.printf("%d\t %f%n", 800, benchmarkDuplicateSearch(800, 800));
+        System.out.printf("%d\t %f%n", 1600, benchmarkDuplicateSearch(1600, 1600));
+        System.out.printf("%d\t %f%n", 3200, benchmarkDuplicateSearch(3200, 3200));
+        System.out.printf("%d\t %f%n", 6400, benchmarkDuplicateSearch(6400, 6400));
+        System.out.printf("%d\t %f%n", 12800, benchmarkDuplicateSearch(12800, 12800));
+
+        //benchmark better duplicate search
+//        System.out.printf("%d\t %f%n", 100, benchmarkBetterDuplicateSearch(100,100));
+//        System.out.printf("%d\t %f%n", 200, benchmarkBetterDuplicateSearch(200,200));
+//        System.out.printf("%d\t %f%n", 400, benchmarkBetterDuplicateSearch(400,400));
+//        System.out.printf("%d\t %f%n", 800, benchmarkBetterDuplicateSearch(800,800));
+//        System.out.printf("%d\t %f%n", 1600, benchmarkBetterDuplicateSearch(1600,1600));
+//        System.out.printf("%d\t %f%n", 3200, benchmarkBetterDuplicateSearch(3200,3200));
+//        System.out.printf("%d\t %f%n", 6400, benchmarkBetterDuplicateSearch(6400,6400));
+//        System.out.printf("%d\t %f%n", 12800, benchmarkBetterDuplicateSearch(12800,12800));
 
     }
 
@@ -37,21 +58,45 @@ public class Main {
     public static void betterDuplicateSearch(int[] sortedArray1, int[] sortedArray2) {
         int index1 = 0;
         int index2 = 0;
-        while (index1 < (sortedArray1.length)) {
+        while (index1 < (sortedArray1.length) && index2 < sortedArray2.length) {
             if (sortedArray2[index2] < sortedArray1[index1]) {
                 index2++;
             } else if (sortedArray1[index1] == sortedArray2[index2]) {
                 index1++;
-                System.out.println("duplicate found! " + sortedArray1[index1 - 1] + sortedArray2[index2]);
-                if (index1 == sortedArray1.length - 1) {
-                    break;
-                }
+//                System.out.println("duplicate found! " + sortedArray1[index1 - 1] + sortedArray2[index2]);
+
             } else if (sortedArray1[index1] < sortedArray2[index2]) {
                 index1++;
             }
-            System.out.println("no more duplicates");
-            System.out.println(index1);
+//            System.out.println("no more duplicates");
+//            System.out.println(index1);
         }
+    }
+
+    public static double benchmarkDuplicateSearch(int arraySize1, int arraySize2) {
+        int[] sortedArray1 = createSortedArray(arraySize1);
+        int[] sortedArray2 = createSortedArray(arraySize2);
+        double sum = 0;
+        for (int i = 0; i < 100_000; i++) {
+            // duplicateSearch(sortedArray1,sortedArray2);
+            long timeStart = System.nanoTime();
+            duplicateSearch(sortedArray1, sortedArray2);
+            sum += (double) (System.nanoTime() - timeStart);
+        }
+        return sum / 100_000;
+    }
+
+    public static double benchmarkBetterDuplicateSearch(int arraySize1, int arraySize2) {
+        int[] sortedArray1 = createSortedArray(arraySize1);
+        int[] sortedArray2 = createSortedArray(arraySize2);
+        double sum = 0;
+        for (int i = 0; i < 100_000; i++) {
+//             betterDuplicateSearch(sortedArray1,sortedArray2);
+            long timeStart = System.nanoTime();
+            betterDuplicateSearch(sortedArray1, sortedArray2);
+            sum += (double) (System.nanoTime() - timeStart);
+        }
+        return sum / 100_000;
     }
 
     public static boolean binarySearch(int[] array, int key) {
