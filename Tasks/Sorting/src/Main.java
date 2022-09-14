9b9010d2870611ff5e05d569ef0868ff22248423
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-09-14 16:12:21 +0200

    insertion sort completed

--- Tasks/Sorting/src/Main.java
+++ Tasks/Sorting/src/Main.java
@@ -2,20 +2,22 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        SelectionSort selection = new SelectionSort();
         int[] array = createRandomArray(20);
-        array = selection.selectionShort(array);
+        Sort.insertion(array);
         for (int i : array) {
             System.out.printf("%d ", i);
         }
         System.out.println();
     }
-/**
- * creates an array of specified size with random numbers. Can contain duplicates.
- * @param arraySize the desired size
- * @return an unsorted array.
- */
-    private static int[] createRandomArray(int arraySize){
+
+    /**
+     * creates an array of specified size with random numbers. Can contain
+     * duplicates.
+     * 
+     * @param arraySize the desired size
+     * @return an unsorted array.
+     */
+    private static int[] createRandomArray(int arraySize) {
         Random rnd = new Random();
         int[] rndArray = new int[arraySize];
         for (int i = 0; i < rndArray.length; i++) {
@@ -23,6 +25,5 @@ public class Main {
         }
         return rndArray;
     }
-
 
 }
