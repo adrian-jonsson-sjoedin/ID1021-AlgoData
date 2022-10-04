3ed94a358b9d77cf0e6401010331ecc41b10b6d2
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-10-04 21:39:28 +0200

    working array queue. haven't implemented shrinking of array

--- Tasks/Queues/src/ArrayQueue.java
+++ Tasks/Queues/src/ArrayQueue.java
@@ -15,76 +15,126 @@ public class ArrayQueue<Item> {
     }
 
     private boolean isFull() {
-        boolean full = false;
-
-        return (this.last == this.first && this.queue[0] != null);
+        return this.itemsInQueue == this.size;
     }
 
     public void enqueue(Item item) {
-        if (this.itemsInQueue == this.queue.length) {
-            System.out.println("queue full. resize or wrap around");
-        }
-
-        if (!isEmpty() && this.last != (this.queue.length - 1)
-                && this.queue[this.last] != null) {
-            this.last++;
-            this.queue[this.last] = item;
-            this.itemsInQueue++;
-        }
-        if (this.last == this.queue.length - 1 && this.first != 0 && !isFull()) {
-            this.last = 0;
-            this.queue[this.last] = item;
-            this.itemsInQueue++;
-
-        }
         if (isEmpty()) {
             this.queue[this.first] = item;
             this.itemsInQueue++;
+            this.last++;
+            return;
+        }
+        if (!isEmpty()) {
+            if ((this.last - 1) == (this.size - 1) && !isFull()) {
+                this.last = 0;
+            }
+            if (isFull()) {
+                resize();
+
+            }
+            if (this.last == 0) {
+                this.queue[this.last] = item;
+                this.last++;
+                this.itemsInQueue++;
+            } else {
+                this.queue[this.last] = item;
+                this.last++;
+                this.itemsInQueue++;
+            }
         }
     }
 
     public Item dequeue() {
         if (isEmpty()) {
-            System.out.println("enqueue(): queue is empty");
+            System.out.println("dequeue(): queue is empty");
             return null;
         }
         Item returnedItem = this.queue[this.first];
         this.queue[this.first] = null;
         this.first++;
         this.itemsInQueue--;
+
         // check if first has passed last. if it has it means all items are dequeued
         // we can then start filling it up from the start again.
-        if (this.first > this.last && this.queue[0] == null)
-            this.first = this.last = 0;
+
+        // if (this.first > this.last && this.queue[0] == null)
+        // this.first = this.last = 0;
         return returnedItem;
     }
 
     public void print() {
         System.out.println("Start of array");
-        for (int i = 0; i < this.queue.length; i++) {
+        for (int i = 0; i < this.size; i++) {
             System.out.println(queue[i]);
         }
     }
 
+    public void resize() {
+        int newSize = this.size * 2;
+        Item[] newQueue = (Item[]) new Object[newSize];
+        int j = 0;
+        for (int i = this.first; i < this.size; i++) {
+            newQueue[j] = this.queue[i];
+            j++;
+        }
+        for (int i = 0; i < this.first; i++) {
+            newQueue[j] = this.queue[i];
+            j++;
+        }
+        this.queue = newQueue;
+        this.size = newSize;
+        this.first = 0;
+        this.last = j;
+        newQueue = null;
+    }
+
     public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue<Integer>(3);
+        ArrayQueue queue = new ArrayQueue<Integer>(4);
         queue.enqueue(0);
         queue.enqueue(1);
         queue.enqueue(2);
-        queue.print();
-        System.out.println("retrieved item: " + queue.dequeue());
-        queue.print();
         queue.enqueue(3);
         queue.print();
-        System.out.println("retrieved item: " + queue.dequeue());
-        queue.print();
-        System.out.println("retrieved item: " + queue.dequeue());
-        queue.print();
         queue.enqueue(4);
-        queue.print();
         queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
         queue.print();
+        System.out.println("retrieved item: " + queue.dequeue());
+        System.out.println("retrieved item: " + queue.dequeue());
+        System.out.println("retrieved item: " + queue.dequeue());
+        queue.enqueue(8);
+        queue.enqueue(9);
+        queue.enqueue(10);
+        queue.print();
+        queue.enqueue(11);
+        queue.enqueue(12);
+        queue.enqueue(13);
+        queue.enqueue(14);
+        queue.enqueue(15);
+        queue.enqueue(16);
+        queue.enqueue(17);
+        queue.enqueue(18);
+        queue.print();
+        System.out.println("retrieved item: " + queue.dequeue());
+        System.out.println("retrieved item: " + queue.dequeue());
+        System.out.println("retrieved item: " + queue.dequeue());
+        queue.enqueue(19);
+        queue.enqueue(20);
 
+        queue.print();
+        // queue.enqueue(0);
+        // queue.enqueue(1);
+        // queue.enqueue(2);
+        // queue.enqueue(3);
+        // queue.enqueue(4);
+        // queue.print();
+        // System.out.println("retrieved item: " + queue.dequeue());
+        // System.out.println("retrieved item: " + queue.dequeue());
+        // System.out.println("retrieved item: " + queue.dequeue());
+        // System.out.println("retrieved item: " + queue.dequeue());
+        // queue.print();
+        // System.out.println("retrieved item: " + queue.dequeue());
     }
-
 }
