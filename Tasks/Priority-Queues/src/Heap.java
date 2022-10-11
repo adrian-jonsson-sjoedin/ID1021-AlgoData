2e39228b949f7e6fca47511da9fbb4f3f2133c2a
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-10-11 21:49:19 +0200

    working on add

--- Tasks/Priority-Queues/src/Heap.java
+++ Tasks/Priority-Queues/src/Heap.java
@@ -1,38 +1,18 @@
 public class Heap<P extends Comparable, T> {
     Node root;
 
-    public class Node {
-        public P priority;
-        public T value;
-        public int size;
-        public Node left;
-        public Node right;
+    private class Node {
+        private P priority;
+        private T value;
+        private int size;
+        private Node left;
+        private Node right;
 
-        private Node(P priority, T value, int size) {
+        public Node(P priority, T value, int size) {
             this.priority = priority;
             this.value = value;
             this.size = size;
             this.left = this.right = null;
-        }
-
-        private void add(P priority, T item) {
-            if (this.priority == priority) {
-                System.out.println("There already is an item with this priority in the 
-                queue. Please choose another priority for this");
-            }
-            if (this.key > k) {
-                if (this.left == null) {
-                    this.left = new Node(k, v);
-                } else {
-                    this.left.add(k, v);
-                }
-            } else {
-                if (this.right == null) {
-                    this.right = new Node(k, v);
-                } else {
-                    this.right.add(k, v);
-                }
-            }
         }
 
         public void print() {
@@ -49,15 +29,17 @@ public class Heap<P extends Comparable, T> {
     }
 
     public void add(P priority, T item) {
+        int temp;
         if (root == null) { // if tree is empty we add the first item as root and it will have a size of 1
             root = new Node(priority, item, 1);
-        } else {
-            root.add(priority, item);
+            return;
         }
-    }
-
-    public Integer lookup(Integer key) {
-        return root.lookup(key);
+        root.size++; // increment the size of the first node, i.e. the root by one
+        Node current = this.root;
+        if (priority.compareTo(root.priority) < 0) { // priority.compareTo(root.priority) = -1 if priority <
+                                                     // root.priority
+            // new item should be placed as root
+        }
     }
 
     public void print() {
