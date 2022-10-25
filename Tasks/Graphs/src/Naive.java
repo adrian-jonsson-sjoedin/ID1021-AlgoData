aa54b015e88044e0815e106a5312e4e21cd83f7c
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-10-25 21:03:02 +0200

    Linked list implementaion of Naive done

--- Tasks/Graphs/src/Naive.java
+++ Tasks/Graphs/src/Naive.java
@@ -24,12 +24,57 @@ public class Naive {
 
     public static void main(String[] args) {
         String[] input = new String[4];
-        input[0] = "Stockholm";
-        input[1] = "Malmö";
-        input[2] = "800";
+        int cities = 7;
+        switch (cities) {
+            case 0 -> {
+                input[0] = "Malmö";
+                input[1] = "Göteborg";
+                input[2] = "500";
+            }
+            case 1 -> {
+                input[0] = "Göteborg";
+                input[1] = "Stockholm";
+                input[2] = "500";
+            }
+            case 2 -> {
+                input[0] = "Malmö";
+                input[1] = "Stockholm";
+                input[2] = "500";
+            }
+            case 3 -> {
+                input[0] = "Stockholm";
+                input[1] = "Sundsvall";
+                input[2] = "500";
+            }
+            case 4 -> {
+                input[0] = "Stockholm";
+                input[1] = "Umeå";
+                input[2] = "700";
+            }
+            case 5 -> {
+                input[0] = "Göteborg";
+                input[1] = "Sundsvall";
+                input[2] = "600";
+            }
+            case 6 -> {
+                input[0] = "Sundsvall";
+                input[1] = "Umeå";
+                input[2] = "500";
+            }
+            case 7 -> {
+                input[0] = "Umeå";
+                input[1] = "Göteborg";
+                input[2] = "1000";
+            }
+            case 8 -> {
+                input[0] = "Göteborg";
+                input[1] = "Umeå";
+                input[2] = "1000";
+            }
+        }
         Map map = new Map("/home/adrian/KTH/ID1021-AlgoData/Tasks/Graphs/src/trains.csv");
         Integer max = Integer.valueOf(input[2]);
-        System.out.println(max);
+        // System.out.println(max);
         long t0 = System.nanoTime();
         Integer distance = shortest(map.lookupOrAdd(input[0]), map.lookupOrAdd(input[1]), max);
         long t1 = System.nanoTime();
